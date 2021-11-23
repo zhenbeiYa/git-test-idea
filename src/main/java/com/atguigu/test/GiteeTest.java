c95d0e313994c2148c9cd620d467c5443607c5c3
zhenbeiYa <zhenbeiYa@github.com>
2021-11-23 15:44:13 +0800

    conclusion update

--- src/main/java/com/atguigu/test/GiteeTest.java
+++ src/main/java/com/atguigu/test/GiteeTest.java
@@ -6,9 +6,16 @@ package com.atguigu.test;/*
  *@Version 1.0
  * */
 
+import java.util.Scanner;
+
 public class GiteeTest {
     public static void main(String[] args) {
         System.out.println("gitee good");
         System.out.println("gitee goods");
+        Scanner scanner=new Scanner(System.in);
+         int count =scanner.nextInt();
+        if(count==1){
+            System.out.println("成功推送");
+        }
     }
 }
