5986ca0fec7fd3eed2e86c0952d144e092252786
zhenbeiYa <zhenbeiYa@github.com>
2021-11-23 15:24:06 +0800

    first  git commit

---
+++ src/main/java/com/atguigu/test/GiteeTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;/*
+ * @ClassName GiteeTest
+ *@Description TODO
+ *@Author 23133
+ *@Date 2021/11/23 15:22
+ *@Version 1.0
+ * */
+
+public class GiteeTest {
+    public static void main(String[] args) {
+        System.out.println("gitee good");
+    }
+}
