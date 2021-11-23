eec179b16f9207c70b2aefc0f0b87fd8e2981358
ましろ(黄建亮) <70832353+zhenbeiYa@users.noreply.github.com>
2021-11-23 15:40:40 +0800

    print update

--- src/main/java/com/atguigu/test/GiteeTest.java
+++ src/main/java/com/atguigu/test/GiteeTest.java
@@ -9,5 +9,6 @@ package com.atguigu.test;/*
 public class GiteeTest {
     public static void main(String[] args) {
         System.out.println("gitee good");
+        System.out.println("gitee goods");
     }
 }
