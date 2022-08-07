d58ecfb6ecc5ffa209ef18e3a10ece843b2dc5a5
shiina <shiina@134.com>
2022-08-07 17:57:16 +0800

    first IDEA Git commit

---
+++ src/main/java/com/atguigu/git/GitApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitApplication
+ * @Description TODO
+ * @Author hjlSya
+ * @Date 2022/8/7
+ **/
+
+public class GitApplication {
+    public static void main(String[] args) {
+        System.out.println("git hello");
+    }
+}
