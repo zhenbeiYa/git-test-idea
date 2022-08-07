86b5cec150a219f539210208b1b591176a28da84
shiina <shiina@134.com>
2022-08-07 18:11:00 +0800

    dev Second IDEA Git commit

--- src/main/java/com/atguigu/git/GitApplication.java
+++ src/main/java/com/atguigu/git/GitApplication.java
@@ -11,5 +11,6 @@ public class GitApplication {
     public static void main(String[] args) {
         System.out.println("git hello");
         System.out.println("git hello3");
+        System.out.println("git hello4!");
     }
 }
